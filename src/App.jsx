20406fc0df2c6df8f3348acc43c57406e8d71ec4
BishalDey908 <ronydey34567@gmail.com>
2023-10-06 12:17:38 +0530

    1st Commit

---
+++ src/App.jsx
@@ -0,0 +1,24 @@
+import Home from "./Components/Home"
+import "./App.css"
+import About from "./Components/About"
+import Work from "./Components/Work"
+import Testimonials from "./Components/Testimonials"
+import Contact from "./Components/Contact"
+import Footer from "./Components/Footer"
+
+
+function App() {
+  
+  return (
+    <>
+    <Home/>
+    <About/>
+    <Work/>
+    <Testimonials/>
+    <Contact/>
+    <Footer/>
+    </>
+  )
+}
+
+export default App
